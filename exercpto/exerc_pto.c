592315cdfad110004984125f1177d3480292b82d
LUcas Martins Mendes <lucas.martinsmendes@gmail.com>
2018-03-30 23:46:44 -0300

    one afternoon of work (forgot to split into small commits)

--- exercpto/exerc_pto.c
+++ exercpto/exerc_pto.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 /*
 4) Crie um sistema de vendas de ferramentas que tenha as seguintes
@@ -12,35 +14,186 @@ realizar vendas, consultar relatório de vendas por cliente, consultar
 relatório de vendas em ordem crescente por valor
 - O sistema deve armazenar os dados em disco
 */
+#define DEBUG
 
 #include "exerc_pto.h"
 
+client_t  client_head = { .next=NULL};
+tool_t   tool_head   = { .next=NULL };
+sale_c_t  sale_c_head = { .next=NULL };
 
+client_t  *tmp_client;
+tool_t    *tmp_tool;
+sale_c_t  *tmp_sale_c;
 
-/**
- * Insert element into linked list.
- * @param  toinsert [description]
- * @param  head     [description]
- * @return          NULL if unable to insert element
- */
-base_t * insert(base_t * head, uint8_t size)
-{
-  base_t * elem;
-  while(elem->)
-};
+unsigned int _opt[4] = {0,0,0,0};
 
-
-union foobaz fb= { .f.x=1 };
-struct foo *ptr = (struct foo*)&fb;
 
 int main(void)
 {
-  printf("%d\n",sizeof(union foobaz));
-  printf("%d\n",ptr->x);
-  /*
-  while(1)
+  puts("Hello, initializing\n last_compiled:"__DATE__);
+  while (1)
   {
+    printf("which functionalities do you want to access?\n");
+    for(uint8_t i=0;menu_opt[i][0][0];i++)
+    {
+      for(uint8_t j=0;menu_opt[i][j][0];j++)
+      {
+        printf("* %s [%u]\n",menu_opt[i][j],j);
+      }
+      //fflush(stdout);
+      scanf("%u",&_opt[i]);
+      if(_opt[i]==SYNC){break;}
+    }
+    #ifdef DEBUG
+    printf("%u %u\n",_opt[0],_opt[1]);
+    #endif
 
-  }*/
+    switch (_opt[0])
+    {
+      case TOOL:
+
+      break;
+      case CLIENT:
+        switch(_opt[1])
+        {
+          case ADD:
+            tmp_client=malloc(sizeof(generic_t));
+            if(tmp_client==NULL)
+            {
+              ERR("mem");
+              break;
+            }
+            insert((generic_t *)&client_head,(generic_t *)tmp_client);
+            puts("insert the client data\n");
+            puts("name:%s");
+            scanf("%s\n",tmp_client->name);
+            puts("surname:%s");
+            scanf("%s\n",tmp_client->surname);
+            puts("id_code:%u");
+            scanf("%u", &tmp_client->id_code);
+            puts("cpf:"CPF_FMT);
+            scanf(CPF_FMT,CPF_ARGS(&tmp_client->cpf));
+            puts("rg"RG_FMT);
+            scanf(RG_FMT,RG_ARGS(&tmp_client->rg));
+          break;
+          case LIST:
+            LIST_ITER(client_head,tmp_client, CLIENT_FMT,CLIENT_ARGS(tmp_client));
+          break;
+          case DELETE:
+            puts("insert client id to delete");
+            uint32_t tofindcode;
+            scanf("%u",&tofindcode);
+            tmp_client=(client_t *)find((generic_t *)&client_head,tofindcode);
+            if(tmp_client)
+            {
+              printf("deleting\n\t "CLIENT_FMT,CLIENT_ARGS(tmp_client));
+              delete((generic_t *)&client_head,(generic_t *)tmp_client);
+              puts("done.\n");
+            }
+            else
+            {
+              ERR("no such client");
+            }
+          break;
+          default:
+              ERR("invalid opt");
+          break;
+        }
+      break;
+      #if 0
+      case SALES:
+        switch(_opt[1])
+        {
+          case ADD:
+            tmp_sale_c=malloc(sizeof(generic_t));
+            if(tmp_sale_c==NULL)
+            {
+              ERR("mem");
+              break;
+            }
+            insert((generic_t *)&sales_c_head,(generic_t *)tmp_sales_c);
+            puts("insert the sale info\n");
+            puts("id_code:%s");
+            scanf("%s\n",tmp_sales_c->name);
+            puts("surname:%s");
+            scanf("%s\n",tmp_sales_c->surname);
+            puts("id_code:%u");
+            scanf("%u", &tmp_sales_c->id_code);
+            puts("cpf:"CPF_FMT);
+            scanf(CPF_FMT,CPF_ARGS(tmp_sales_c->cpf));
+            puts("rg"RG_FMT);
+            scanf(RG_FMT,RG_ARGS(tmp_sales_c->rg));
+          break;
+          case LIST:
+            LIST_ITER(client_head,tmp_client,\
+              "id_code:%u",\
+          break;
+          case DELETE:
+            /*TODO use find to find item to delete by id code*/
+            puts("insert client id to delete");
+            uint32_t tofindcode;
+            scanf("%u",&tofindcode);
+            tmp_client=(client_t *)find((generic_t *)&client_head,tofindcode);
+            if(tmp_client)
+            {
+              printf("deleting\n\tname:%s surname:%s id_code:%u cpf:"CPF_FMT"rg"RG_FMT,\
+              tmp_client->name,tmp_client->surname,tmp_client->id_code,\
+              tmp_client->cpf[0],tmp_client->cpf[1],tmp_client->cpf[3],tmp_client->cpf[4],\
+              tmp_client->rg[0],tmp_client->rg[1],tmp_client->rg[2]);
+
+              delete((generic_t *)&client_head,(generic_t *)tmp_client);
+              puts("done.\n");
+            }
+            else
+            {
+              ERR("no such client");
+            }
+      break;
+      #endif
+      case SYNC:
+
+      break;
+      default:
+          ERR("invalid opt");
+      break;
+      }
+    }
+  return 0;
+}
+
+generic_t * find(generic_t * head, uint32_t tofind_id)
+{
+  base_t *elem= (base_t *)head;
+}
+
+void insert(generic_t * head, generic_t * _toinsert)
+{
+  base_t * elem= (base_t *)head;
+  base_t * toinsert= (base_t *)_toinsert;
+  for (;elem->next;elem=elem->next); /* go to list tail */
+  elem->next=toinsert;
+  toinsert->next=NULL;
+};
+
+int delete(generic_t * head, generic_t * todelete)
+{
+  base_t * elem= (base_t *)head;
+  base_t * bef;/*the element before the node to be deleted*/
+
+  for(;elem->next&&elem->next!=todelete/*stop when next equal todelete*/;elem=elem->next);
+  if(!elem->next) return -1; /* did not found the element  in the list, simple sanity check*/
+  bef=elem;
+  elem=elem->next;/* now elem == todelete*/
+  if(elem->next==NULL)
+  {
+    elem->next=NULL;
+    free(todelete);
+  }
+  else
+  {
+    bef->next=elem->next;/*set next pointer of before todelete*/
+    free(todelete);
+  }
   return 0;
 }
--- exercpto/exerc_pto.h
+++ exercpto/exerc_pto.h
@@ -6,83 +6,155 @@
 #ifndef EXERC_PTO
 #define EXERC_PTO
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+
+/*--------Macros-------*/
+
+#define _STR(s) #s
+#define STRINGIFY(s) _STR(s)
+
+#define _TOKENPASTE(x,y) x # y
+#define TOKENPASTE(x,y) _TOKENPASTE(x,y)
+
+#define ERR(str) printf(str" ERR at "STRINGIFY(LINE)" in %s",__func__)
+
+ /**
+ * print struct contents.
+ * @param head  head of list
+ * @param param list of variable addresses for printing
+ * @param fmt   [description]
+ * @param iteration element
+ */
+ #ifndef LIST_ITER
+ #define LIST_ITER(head,iterel,fmt,...) \
+ for(iterel=&head;iterel->next;iterel=iterel->next)   \
+ {                                  \
+     printf(fmt,__VA_ARGS__);       \
+ }
+#endif /* LIST_ITER */
+
+/*----------Constants---------*/
+
 #define BUFF_LEN 100 /*!<default buffer lenght*/
 
-/**
- * @struct base_struct
- * base struct containing just the next pointer
- */
-typedef struct base_struct {
-  struct base_struct *next;
-}base_t;
+#define CPF_FMT "%hu.%hu.%hu-%hu"
+#define CPF_ARGS(varname) varname[0],varname[1],varname[3],varname[4]
 
+#define RG_FMT "%hu.%hu.%hu"
+#define RG_ARGS(varname) varname[0],varname[1],varname[3]
 
+#define CLIENT_FMT "id_code:%u name:%s surname:%s cpf:"CPF_FMT" rg:"RG_FMT
+#define CLIENT_ARGS(var) var->id_code,var->name,var->surname,\
+                          CPF_ARGS(var->cpf),RG_ARGS(var->rg)
+
+const char menu_opt[3][5][20] =
+  {
+    {"Tool stock control","client database","sales database","sync to disk",'\0'},
+    {"add","list","delete",'\0'},
+    {'\0'}
+  };
+
+enum menu_l0
+{
+  TOOL=0,
+  CLIENT,
+  SALES,
+  SYNC
+};
+
+enum menu_l1
+{
+  ADD=0,
+  LIST,
+  DELETE
+};
+
+/*----------data structures---------*/
 /**
  * @struct _tool
  * tool struct.
  */
-struct _tool
+typedef struct _tool
 {
- uint32_t code;/*!<id code*/
+  void * next;
+ uint32_t id_code;/*!<id code*/
  char desc[BUFF_LEN];/*!<name with description*/
  uint32_t price;/*!<price*/
-};
-
-/**
- * @union tool_node
- * node for inserting in linked lists.
- */
-typedef union tool_node
-{
-  base_t base;
-  struct _tool tool;
 }tool_t;
-
 
 
 /**
  * @struct _client
  * client identification struct.
  */
-struct _client
+typedef struct _client
 {
+  void * next;
   uint32_t id_code;
   char name[BUFF_LEN];
   char surname[BUFF_LEN];
   uint16_t cpf[4];
-  const char cpf_fmt[12] = "%u.%u.%u-%u";
   uint16_t rg[3];
-  const char rg_fmt[9] = "%u.%u.%u";
-} client_t;
-
-/**
- * @union client_node
- * client struct for using with linked lists.
- */
-typedef union client_node
-{
-  base_t base;
-  struct _client client;
 }client_t;
 
+
 /**
+ * @struct _sale_c
  * Sale control struct.
  */
-struct sale_control
+typedef struct _sale_c
 {
-  uint32_t code;
+  void * next;
+  uint32_t id_code;
   uint32_t tool_code;
   uint32_t client_code;
   uint32_t value;
-  const char param_name[];
-};
+}sale_c_t;
+
+typedef struct _base
+{
+  void * next;
+  uint32_t id_code;
+}base_t;
+/**
+ * @union _generic
+ * union for casting into when inserting into lists.
+ */
+typedef union generic
+{
+  struct _base base;
+  struct _tool tool;
+  struct _client client;
+  struct _sale_c sales;
+}generic_t;
 
 /**
  * Insert element into linked list.
- * @param  toinsert [description]
- * @param  head     [description]
- * @return          NULL if unable to insert element
+ * @param  head     list head
+ * @param  toinsert element to insert into list tail
+ * @return
  */
-base_t * insert(base_t * head, uint8_t size)
+void insert(generic_t * head, generic_t * toinsert);
+
+/**
+ * delete element from list and re-link list pointers.
+ * @param  head     the list head
+ * @param  todelete to delete
+ * @return          -1 on err
+ */
+int delete(generic_t * head, generic_t * todelete);
+
+/**
+ * find object by id in linked list.
+ * @param  head      list head
+ * @param  tofind_id id to find
+ * @return           [description]
+ */
+generic_t * find(generic_t * head, uint32_t tofind_id);
+
 
 #endif /* EXERC_PTO */
